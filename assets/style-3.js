7748725843f39e2c214aa1b75605bef3a8ba98b2
Alejandro Buendia <alb2281@columbia.edu>
2024-04-24 10:25:43 -0400

    Updates

--- assets/style-3.js
+++ assets/style-3.js
@@ -14,7 +14,7 @@ const highlightcolor = "#111";
 // Body properties
 const bodyfontweight = 300;
 const bodyfontsize = "12pt";
-const backgroundcolor = "#ffffff";
+const backgroundcolor = "#e6f3f2";
 
 // Link properties
 const acolor = accentcolor;
